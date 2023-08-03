3554e2a005e573b8e0723b2b8e78d7bf9c8e8ee9
ramji <ramjirao11code@gmail.com>
2023-08-03 23:12:57 +0530

    Password Validator

--- WeatherForecast/src/com/forecast/Test.java
+++ WeatherForecast/src/com/forecast/Test.java
@@ -1,19 +1,46 @@
 package com.forecast;
 
-import java.net.MalformedURLException;
-
 
 public class Test {
-
-	public static void main(String[] args) throws MalformedURLException {
 	
-		
 	
-		
-		
+public static void main(String[] args)  {
 
-		 
-		 
-	}
+
+	
+       String pwd="rmji11";
+       
+       boolean ans=isGood(pwd);
+       System.out.println(ans);
+	
+}
+
+public  static boolean isGood(String pwd) {
+	
+  if(pwd.length()<6) return false;
+  
+  
+  boolean hasDigit=false;
+  boolean hasUpper=false;
+  boolean hasLower=false;
+  
+  
+  for(Character c:pwd.toCharArray())
+  {
+	  if(Character.isDigit(c))
+		  hasDigit=true;
+	  else if(Character.isUpperCase(c))
+		  hasUpper=true;
+	  else if(Character.isLowerCase(c))
+		  hasLower=true;
+	  
+	  if(c==' '||c=='/')
+		  return false;
+		  
+  }
+  return hasDigit&&hasUpper&&hasLower;
+  
+}
 
 }
+
