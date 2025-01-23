4d2efd0b10d1b98da6518935b118c1ecc986db0a
ramji117 <ramjirao11code@gmail.com>
2025-01-23 17:14:32 +0530

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Your API URL
+
+            Print(3);
+
+        }
+
+        public static void Print(int number)
+        {
+            Console.WriteLine("Hello World" + number);
+
+        }
+    }
+}
+
+
