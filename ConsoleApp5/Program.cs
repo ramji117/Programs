bacddd47d3d45ff98cb7065985b2c1969c65b24c
ramji117 <ramjirao11code@gmail.com>
2025-01-26 13:10:13 +0530

    DBLayer Utility Class Initial Commit

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NLog;
+using System;
+using System.Collections.Generic;
 
 namespace ConsoleApp5
 {
@@ -7,21 +9,41 @@ namespace ConsoleApp5
         public static void Main(string[] args)
         {
             // Your API URL
+            List<string> list = new List<string>();
+            list.Add("Ramjirao");
+            list.Add("Developer");
 
-            Print(3);
+            foreach (string item in list) {
 
+                {
+                    Console.WriteLine(item);
+                }
+
+                Print(3);
+
+            }
+
+              void Print(int number)
+            {
+                Logger log = LogManager.GetCurrentClassLogger();
+                try
+                {
+                    Console.WriteLine(0);
+                    log.Info("Success");
+
+                }
+                catch (Exception ex)
+                {
+                    {
+                        log.Error("Divide By Zero");
+                    }
+
+
+                }
+            }
         }
 
-        public static void Print(int number)
-        {
-            
-            Console.WriteLine(2);
-            Console.WriteLine(number);
-        
-        }
     }
-
-
 }
 
 
