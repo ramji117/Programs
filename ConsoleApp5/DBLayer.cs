3ccccf56a74f737e17693806e4b16c059c1109db
ramji117 <ramjirao11code@gmail.com>
2025-01-26 13:13:49 +0530

    DBLayer

---
+++ ConsoleApp5/DBLayer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Utility
+{
+    public static class DBLayer
+    {
+        private static readonly object lockObject = new object();
+
+
+        private static string dbConnectionString = null;
+
+        private static SqlConnection sqlConnection = null;
+
+
+        public static SqlConnection GetConnection()
+        {
+            if (string.IsNullOrEmpty(dbConnectionString))
+            {
+                throw new InvalidOperationException("Database connection string is not set.");
+
+            }
+            lock (lockObject)
+            {
+                try
+                {
+                    if (sqlConnection == null || sqlConnection.State == System.Data.ConnectionState.Closed)
+                    {
+                        sqlConnection = new SqlConnection(dbConnectionString);
+                        sqlConnection.Open();
+
+                    }
+                }
+                catch (SqlException ex)
+                {
+                    throw new Exception("Failed to connect to the database. Check the connection string and database server.", ex);
+                }
+                catch (InvalidOperationException ex)
+                {
+                    throw new Exception("Connection string is in an invalid format.", ex);
+                }
+            }
+
+            return sqlConnection;
+        }
+
+        public static void CloseConnection()
+        {
+            try
+            {
+                if (sqlConnection != null && sqlConnection.State == System.Data.ConnectionState.Open)
+                {
+                    sqlConnection.Close();
+                    sqlConnection = null;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Failed To Close Connection", ex);
+            }
+
+        }
+    }
+
+}
